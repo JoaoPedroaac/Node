37873fff7fdd410996bf71756bb96f8e2be1cad7
João Pedro <joao.cintra7@hotmail.com>
2024-11-25 15:28:48 -0300

    Organizando paginas

--- 1_INTRO/2_entendendo_modulo/arquivo.js
+++
@@ -1,1 +0,0 @@
-console.log("Good AfterMoon Node.js!");
--- index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');       // Importa o módulo fs
-
-fs.readFile('arquivo.txt', 'utf8', (err, data) => {    // Lê o arquivo.txt
-    if (err) {
-        console.error(err)
-    return    
-    }
-    console.log(data);
-});
